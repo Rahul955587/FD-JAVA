d3e4c3f8b3238e280f09f05630fd3f6ad1a141ad
Rahul_1239 <rahul955587@gmail.com>
2025-08-11 11:00:40 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,13 @@
+public class Variable {
+    public static void main(String[] args) {
+        /* int a=1;
+        int $rAj= 12;
+        int _rSs=  123;
+        System.out.println(a);
+        System.out.println($rAj);
+        System.out.println(_rSs); */
+        int a=50; // Local VAriable
+        int b=60;
+        System.out.println(a+b);
+    }
+}
