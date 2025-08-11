e22f64066d836bdbd006dfecc9bd98e3c11b0313
Rahul_1239 <rahul955587@gmail.com>
2025-08-11 11:04:53 +0530

    Add files via upload

---
+++ Sum_of_Two_Num.java
@@ -0,0 +1,9 @@
+public class Sum_of_Two_Num {
+    public static void main(String[] args) {
+        int a=500;
+        int b=1;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+    
+}
